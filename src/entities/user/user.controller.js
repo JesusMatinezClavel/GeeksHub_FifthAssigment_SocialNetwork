87901b8488fe60b3adfe6fc0358014efbd5eda5b
JesusMartinezClavel <jmcvalles@gmail.com>
2024-03-13 19:29:45 +0100

    FEAT -> Created updateOwnProfile controller to update your own profile only if there's an input in req.body

--- src/entities/user/user.controller.js
+++ src/entities/user/user.controller.js
@@ -1,5 +1,6 @@
 import { catchStatus, tryStatus } from "../../utils/resStatus.js";
 import User from "./User.js";
+import bcrypt from "bcrypt";
 
 export const getUser = async (req, res) => {
     try {
@@ -10,7 +11,7 @@ export const getUser = async (req, res) => {
     }
 }
 
-export const ownProfile = async (req, res) => {
+export const getOwnProfile = async (req, res) => {
     try {
         const userId = req.tokenData.userID
         const user = await User.findOne({ _id: userId })
@@ -31,6 +32,88 @@ export const ownProfile = async (req, res) => {
         catchStatus(res, `CANNOT CALL OWN PROFILE`, error)
     }
 }
+
+export const updateOwnProfile = async (req, res) => {
+    try {
+        const userId = req.tokenData.userID
+        let { firstName, lastName, profileImg, bio, nickName, email, password, passwordCheck } = req.body
+        const userBefore = await User.findOne({ _id: userId })
+
+        if (firstName === "") {
+            firstName = userBefore.firstName
+        }
+        if (lastName === "") {
+            lastName = userBefore.lastName
+        }
+        if (profileImg === "") {
+            profileImg = userBefore.profileImg
+        }
+        if (bio === "") {
+            bio = userBefore.bio
+        }
+        if (nickName === "") {
+            nickName = userBefore.nickName
+        }
+        if (email === "") {
+            email = userBefore.email
+        }
+        const validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        if (!validEmail.test(email)) {
+            return res.status(400).json(
+                {
+                    success: false,
+                    message: "format email invalid"
+                }
+            )
+        }
+        if (password === "") {
+            password = userBefore.password
+        } else if (password !== passwordCheck) {
+            return res.status(400).json({
+                success: false,
+                message: `passwordCheck is different from password!`
+            })
+        }
+        
+
+        const passwordHash = bcrypt.hashSync(password,8)
+
+
+
+        await User.findOneAndUpdate(
+            {
+                _id: userId
+            },
+            {
+                firstName: firstName,
+                lastName: lastName,
+                nickName: nickName,
+                profileImg: profileImg,
+                bio: bio,
+                email: email,
+                password: passwordHash,
+              }
+        )
+
+        const userAfter = await User.findOne({ _id: userId })
+        const userUpdate = {
+            name: `${userAfter.firstName} ${userAfter.lastName}`,
+            profileImg: userAfter.profileImg,
+            nickName: userAfter.nickName,
+            age: userAfter.age,
+            email: userAfter.email,
+            chats: userAfter.chat.length,
+            liked: userAfter.liked.length,
+            posts: userAfter.posts.length,
+            followers: userAfter.followers.length,
+            follows: userAfter.followed.length
+        }
+        tryStatus(res, `profile updated succesfully`, userUpdate)
+    } catch (error) {
+        catchStatus(res, `CANNOT UPDATE OWN PROFILE`, error)
+    }
+}
+
 
 export const follow = async (req, res) => {
     try {
--- src/entities/user/user.routes.js
+++ src/entities/user/user.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { auth } from "../../middlewares/auth.middleware.js";
-import { follow, getUser, ownProfile, unFollow } from "./user.controller.js";
+import { follow, getUser, getOwnProfile, unFollow, updateOwnProfile } from "./user.controller.js";
 import { isSuperadmin } from "../../middlewares/isSuperadmin.middleware.js";
 
 const router = Router();
 
 router.get('/', auth, isSuperadmin, getUser)
-router.get('/profile', auth, ownProfile)
+router.get('/profile', auth, getOwnProfile)
+router.put('/profile', auth, updateOwnProfile)
 router.put('/follow/', auth, follow)
 router.put('/unfollow/', auth, unFollow)
 
