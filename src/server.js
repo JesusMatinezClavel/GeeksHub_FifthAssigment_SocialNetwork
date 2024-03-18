cce93adebc1018152ee1da61f00cc3da2ad2d4ad
JesusMartinezClavel <jmcvalles@gmail.com>
2024-03-18 20:49:06 +0100

    INSTALL -> Installed the cors package

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ import express from "express";
 import 'dotenv/config'
 import { dbConection } from "./database/db.js";
 import router from "./routes/router.js";
+import cors from "cors";
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 const PORT = process.env.PORT || 4001
